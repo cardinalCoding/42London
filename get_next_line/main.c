ef2378a85f1b1fdedcac7626980efa8201be138e
cardinalCoding <85232444+cardinalCoding@users.noreply.github.com>
2024-03-11 21:18:41 +0000

    Updated get next line

--- get_next_line/main.c
+++ get_next_line/main.c
@@ -6,14 +6,13 @@
 /*   By: bcorte-r <bcorte-r@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 05:24:36 by bcorte-r          #+#    #+#             */
-/*   Updated: 2023/12/13 07:40:36 by bcorte-r         ###   ########.fr       */
+/*   Updated: 2024/01/25 15:29:14 by bcorte-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <fcntl.h>
 #include "get_next_line.h"
-
 
 int	main(void)
 {
@@ -22,7 +21,6 @@ int	main(void)
 	int		count;
 
 	count = 0;
-	line = malloc(1 * sizeof(char));
 	fd = open("example.txt", O_RDONLY);
 	if (fd == -1)
 	{
